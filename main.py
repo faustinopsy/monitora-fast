d24e27ff7512f44c013f7e210089e7a73ca27cea
faustinopsy <rodrigohipnose@gmail.com>
2024-04-09 21:16:28 -0300

    versão mysql

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI, Request
-import uvicorn
+import mysql.connector
 from datetime import datetime
 
 app = FastAPI()
 
-log = "log.txt"
+db_config = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'root123',
+    'database': 'log_fast'
+}
+
+conn = mysql.connector.connect(**db_config)
+cursor = conn.cursor()
+
+@app.on_event("shutdown")
+def shutdown_event():
+    conn.close()
 
 @app.middleware("http")
 async def log_request(request: Request, call_next):
@@ -12,23 +24,24 @@ async def log_request(request: Request, call_next):
     client_ip = request.client.host
     http_method = request.method
     url = request.url.path
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    with open(log, "a") as file:
-        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        escreva_log = f"{timestamp} - HTTP Method: {http_method} | URL: {url} | User-Agent: {user_agent} | Client IP: {client_ip}\n"
-        file.write(escreva_log)
+    sql = "INSERT INTO logs (data, http_method, url, user_agent, client_ip) VALUES (%s, %s, %s, %s, %s)"
+    values = (timestamp, http_method, url, user_agent, client_ip)
+    cursor.execute(sql, values)
+    conn.commit()
 
     response = await call_next(request)
-
     return response
 
 @app.get("/")
-async def inicio():
+async def read_root():
     return {"message": "Olá, mundo!"}
 
 @app.post("/item")
-async def examplo_post():
+async def example_post():
     return {"message": "Exemplo de POST"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
